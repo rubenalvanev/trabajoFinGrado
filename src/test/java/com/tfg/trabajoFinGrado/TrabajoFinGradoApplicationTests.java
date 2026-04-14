e7923c72a2d1b93c78a94df2a228c5c05f1c8cef
rubenalvanev <rubenalvanev@gmail.com>
2026-04-14 16:49:33 +0200

    inicio

---
+++ src/test/java/com/tfg/trabajoFinGrado/TrabajoFinGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.trabajoFinGrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
