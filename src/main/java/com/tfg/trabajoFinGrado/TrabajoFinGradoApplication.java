2d1163c319b17b7ac8f715d37f4b5ad3d03c95dd
ralvarez <rubenalvanev@gmail.com>
2026-04-21 13:11:00 +0200

    tutorial

--- src/main/java/com/tfg/trabajoFinGrado/TrabajoFinGradoApplication.java
+++ src/main/java/com/tfg/trabajoFinGrado/TrabajoFinGradoApplication.java
@@ -3,18 +3,20 @@ package com.tfg.trabajoFinGrado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-@RestController // 1. Añadimos esto
 public class TrabajoFinGradoApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabajoFinGradoApplication.class, args);
 	}
 
-	@GetMapping("/") // 2. Y añadimos este método
-	public String hola() {
-		return "¡Hola mundo!";
-	}
 }
