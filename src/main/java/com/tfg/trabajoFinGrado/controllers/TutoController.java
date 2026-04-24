345e3f7949bb4a8d7bcbcc093b69e039286cb452
ralvarez <rubenalvanev@gmail.com>
2026-04-24 11:53:55 +0200

    prototipo de login

--- src/main/java/com/tfg/trabajoFinGrado/controllers/TutoController.java
+++ src/main/java/com/tfg/trabajoFinGrado/controllers/TutoController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TutoController {
 
-    @GetMapping("/ventana1")
+    @GetMapping("/login")
     public String mostrarVentana1(Model model) {
-        model.addAttribute("mensaje", "Esta es la Ventana 1");
-        return "ventana1"; // Carga ventana1.html
+        model.addAttribute("mensaje", "Login");
+        return "login";
     }
 
     @GetMapping("/ventana2")
